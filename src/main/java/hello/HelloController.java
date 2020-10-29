c9361f39d2efa4b43a8f418b693fc21c46bc84fd
cloudtechmasters <68885738+cloudtechmasters@users.noreply.github.com>
2020-10-29 07:43:16 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,14 +2,15 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 @RestController
 public class HelloController {
-    
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/")
     public String index() {
-        System.out.println("adding something here to fail the build");
-        System.out.println("adding something here to fail the build for jenkins sonar integration");
+        logger.info("adding something here to fail the build");
+        logger.debug("adding something here to fail the build for jenkins sonar integration");
         return "Greetings from Springboot..!!!";
     }
     
